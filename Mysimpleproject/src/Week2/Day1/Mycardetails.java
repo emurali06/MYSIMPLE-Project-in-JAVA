24885c4c57347bea608f75a7b5aef0f15a63d128
Muralie <Muralie@Murali>
2021-08-12 08:20:28 +0530

    Upload allComments

---
+++ Mysimpleproject/src/Week2/Day1/Mycardetails.java
@@ -0,0 +1,17 @@
+package Week2.Day1;
+
+public class Mycardetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int seatcapasity = 4;
+float tankcapacity =5.6F;
+Long EngnieNumber =23459847334566L;
+short Driverseat =1; //short will not not allow more than 5 values 
+System.out.println("Seat Capasity="+seatcapasity);
+System.out.println("Tank Capacity =" +tankcapacity);
+System.out.println("Driver Seat="+Driverseat);
+System.out.println("Engnine Number="+EngnieNumber);
+	}
+
+}
