1a0f6486782bb8377749711dbe53f63229171f12
Muralie <Muralie@Murali>
2021-08-13 06:17:29 +0530

    Mymobile profjet

---
+++ Mysimpleproject/src/Week2/Day1/mobile.java
@@ -0,0 +1,7 @@
+package Week2.Day1;
+
+public class mobile {
+int version =5;
+String brandname ="Samung";
+Float IMEI= 12214349349F;
+}
---
+++ Mysimpleproject/src/Week2/Day1/mymobileupdate.java
@@ -0,0 +1,11 @@
+package Week2.Day1;
+
+public class mymobileupdate {
+public static void main(String[] args) {
+	//Class object = new constructor
+	mobile object = new mobile();
+	System.out.println("Version ="+object.version);
+	System.out.println("Brand NAme="+object.brandname);
+	System.out.println("IMEI Number="+object.IMEI);
+}
+}
